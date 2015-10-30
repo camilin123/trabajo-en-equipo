967b4273a600ce4d65e120b05ffee25470c2b2a7
Chaviups <chaviups17@gmail.com>
2015-10-29 20:48:59 -0600

    Revert "Jejejejejeje"
    This reverts commit 7d13966f4cd9cb8a8b9c54aa467bff261a719dcb.

--- tarea.py
+++ tarea.py
@@ -1,83 +1,37 @@
 
 H1="Hydrogen	1.00794(4)	FFFFFF	1s1	2.20	37		120	1312	-73	-1, 1	gas	diatomic	14	20	8.99E-05	nonmetal	1766"
 He1="Helium	4.002602(2)	D9FFFF	1s2		32		140	2372	0		gas	atomic		4	0	noble gas	1868"
-Li1="Lithium	6.941(2)	CC80FF	[He] 2s1	0.98	134	76 (+1)	182	520	-60	1	solid	metallic	454	1615	0.54	alkali metal	1817"
-Be1="Beryllium	9.012182(3)	C2FF00	[He] 2s2	1.57	90	45 (+2)		900	0	2	solid	metallic	1560	2743	1.85	alkaline earth metal	1798"
-B1="Boron	10.811(7)	FFB5B5	[He] 2s2 2p1	2.04	82	27 (+3)		801	-27	1, 2, 3	solid	covalent network	2348	4273	2.46	metalloid	1807"
-C1="Carbon	12.0107(8)	909090	[He] 2s2 2p2	2.55	77	16 (+4)	170	1087	-154	-4, -3, -2, -1, 1, 2, 3, 4	solid	covalent network	3823	4300	2.26	nonmetal	Ancient"
-N1="Nitrogen	14.0067(2)	3050F8	[He] 2s2 2p3	3.04	75	146 (-3)	155	1402	-7	-3, -2, -1, 1, 2, 3, 4, 5	gas	diatomic	63	77	0	nonmetal	1772"
-O1="Oxygen	15.9994(3)	FF0D0D	[He] 2s2 2p4	3.44	73	140 (-2)	152	1314	-141	-2, -1, 1, 2	gas	diatomic	55	90	0	nonmetal	1774"
-F1="Fluorine	18.9984032(5)	9.00E+51	[He] 2s2 2p5	3.98	71	133 (-1)	147	1681	-328	-1	gas	atomic	54	85	0	halogen	1670"
-Ne1="Neon	20.1797(6)	B3E3F5	[He] 2s2 2p6		69		154	2081	0		gas	atomic	25	27	0	noble gas	1898"
-Na1="Sodium	22.98976928(2)	AB5CF2	[Ne] 3s1	0.93	154	102 (+1)	227	496	-53	-1, 1	solid	metallic	371	1156	0.97	alkali metal	1807"
-Mg1="Magnesium	24.3050(6)	8AFF00	[Ne] 3s2	1.31	130	72 (+2)	173	738	0	1, 2	solid	metallic	923	1363	1.74	alkaline earth metal	1808"
-Al1=	"Aluminum or Aluminium	26.9815386(8)	BFA6A6	[Ne] 3s2 3p1	1.61	118	53.5 (+3)		578	-43	1, 3	solid	metallic	933	2792	2.7	metal	Ancient"
-Si1=	"Silicon	28.0855(3)	F0C8A0	[Ne] 3s2 3p2	1.90	111	40 (+4)	210	787	-134	-4, -3, -2, -1, 1, 2, 3, 4	solid	metallic	1687	3173	2.33	metalloid	1854"
-P1=	"Phosphorus	30.973762(2)	FF8000	[Ne] 3s2 3p3	2.19	106	44 (+3)	180	1012	-72	-3, -2, -1, 1, 2, 3, 4, 5	solid	covalent network	317	554	1.82	nonmetal	1669"
-S1= "Sulfur	32.065(5)	FFFF30	[Ne] 3s2 3p4	2.58	102	184 (-2)	180	1000	-200	-2, -1, 1, 2, 3, 4, 5, 6	solid	covalent network	388	718	1.96	nonmetal	Ancient"
-Cl1="Chlorine	35.453(2)	1FF01F	[Ne] 3s2 3p5	3.16	99	181 (-1)	175	1251	-349	-1, 1, 2, 3, 4, 5, 6, 7	gas	covalent network	172	239	0	halogen	1774"
-Ar1="Argon	39.948(1)	80D1E3	[Ne] 3s2 3p6		97		188	1521	0		gas	atomic	84	87	0	noble gas	1894"
-K1="Potassium	39.0983(1)	8F40D4	[Ar] 4s1	0.82	196	138 (+1)	275	419	-48	1	solid	metallic	337	1032	0.86	alkali metal	1807"
-Ca1="Calcium	40.078(4)	3DFF00	[Ar] 4s2	1.00	174	100 (+2)		590	-2	2	solid	metallic	1115	1757	1.55	alkaline earth metal	Ancient"
-Sc1="Scandium	44.955912(6)	E6E6E6	[Ar] 3d1 4s2	1.36	144	74.5 (+3)		633	-18	1, 2, 3	solid	metallic	1814	3103	2.99	transition metal	1876"
-Ti1="Titanium	47.867(1)	BFC2C7	[Ar] 3d2 4s2	1.54	136	86 (+2)		659	-8	-1, 2, 3, 4	solid	metallic	1941	3560	4.51	transition metal	1791"
-V1="Vanadium	50.9415(1)	A6A6AB	[Ar] 3d3 4s2	1.63	125	79 (+2)		651	-51	-1, 2, 3, 4	solid	metallic	2183	3680	6.11	transition metal	1803"
-Cr1="Chromium	51.9961(6)	8A99C7	[Ar] 3d5 4s1	1.66	127	80 (+2*)		653	-64	-2, -1, 1, 2, 3, 4, 5, 6	solid	metallic	2180	2944	7.14	transition metal	Ancient"
-Mn1="Manganese	54.938045(5)	9C7AC7	[Ar] 3d5 4s2	1.55	139	67 (+2)		717	0	-3, -2, -1, 1, 2, 3, 4, 5, 6, 7	solid	metallic	1519	2334	7.47	transition metal	1774"
-Fe1="Iron	55.845(2)	E06633	[Ar] 3d6 4s2	1.83	125	78 (+2*)		763	-16	-2, -1, 1, 2, 3, 4, 5, 6	solid	metallic	1811	3134	7.87	transition metal	Ancient"
-
+Li="Lithium	6.941(2)	CC80FF	[He] 2s1	0.98	134	76 (+1)	182	520	-60	1	solid	metallic	454	1615	0.54	alkali metal	1817"
+Be="Beryllium	9.012182(3)	C2FF00	[He] 2s2	1.57	90	45 (+2)		900	0	2	solid	metallic	1560	2743	1.85	alkaline earth metal	1798"
+B="Boron	10.811(7)	FFB5B5	[He] 2s2 2p1	2.04	82	27 (+3)		801	-27	1, 2, 3	solid	covalent network	2348	4273	2.46	metalloid	1807"
+C="Carbon	12.0107(8)	909090	[He] 2s2 2p2	2.55	77	16 (+4)	170	1087	-154	-4, -3, -2, -1, 1, 2, 3, 4	solid	covalent network	3823	4300	2.26	nonmetal	Ancient"
+N="Nitrogen	14.0067(2)	3050F8	[He] 2s2 2p3	3.04	75	146 (-3)	155	1402	-7	-3, -2, -1, 1, 2, 3, 4, 5	gas	diatomic	63	77	0	nonmetal	1772"
+O="Oxygen	15.9994(3)	FF0D0D	[He] 2s2 2p4	3.44	73	140 (-2)	152	1314	-141	-2, -1, 1, 2	gas	diatomic	55	90	0	nonmetal	1774"
+F="Fluorine	18.9984032(5)	9.00E+51	[He] 2s2 2p5	3.98	71	133 (-1)	147	1681	-328	-1	gas	atomic	54	85	0	halogen	1670"
+Ne="Neon	20.1797(6)	B3E3F5	[He] 2s2 2p6		69		154	2081	0		gas	atomic	25	27	0	noble gas	1898"
+Na="Sodium	22.98976928(2)	AB5CF2	[Ne] 3s1	0.93	154	102 (+1)	227	496	-53	-1, 1	solid	metallic	371	1156	0.97	alkali metal	1807"
+Mg="Magnesium	24.3050(6)	8AFF00	[Ne] 3s2	1.31	130	72 (+2)	173	738	0	1, 2	solid	metallic	923	1363	1.74	alkaline earth metal	1808"
+Al=	"Aluminum or Aluminium	26.9815386(8)	BFA6A6	[Ne] 3s2 3p1	1.61	118	53.5 (+3)		578	-43	1, 3	solid	metallic	933	2792	2.7	metal	Ancient"
+Si=	"Silicon	28.0855(3)	F0C8A0	[Ne] 3s2 3p2	1.90	111	40 (+4)	210	787	-134	-4, -3, -2, -1, 1, 2, 3, 4	solid	metallic	1687	3173	2.33	metalloid	1854"
+P=	"Phosphorus	30.973762(2)	FF8000	[Ne] 3s2 3p3	2.19	106	44 (+3)	180	1012	-72	-3, -2, -1, 1, 2, 3, 4, 5	solid	covalent network	317	554	1.82	nonmetal	1669"
+S= "Sulfur	32.065(5)	FFFF30	[Ne] 3s2 3p4	2.58	102	184 (-2)	180	1000	-200	-2, -1, 1, 2, 3, 4, 5, 6	solid	covalent network	388	718	1.96	nonmetal	Ancient"
+Cl="Chlorine	35.453(2)	1FF01F	[Ne] 3s2 3p5	3.16	99	181 (-1)	175	1251	-349	-1, 1, 2, 3, 4, 5, 6, 7	gas	covalent network	172	239	0	halogen	1774"
+Ar="Argon	39.948(1)	80D1E3	[Ne] 3s2 3p6		97		188	1521	0		gas	atomic	84	87	0	noble gas	1894"
+K="Potassium	39.0983(1)	8F40D4	[Ar] 4s1	0.82	196	138 (+1)	275	419	-48	1	solid	metallic	337	1032	0.86	alkali metal	1807"
+Ca="Calcium	40.078(4)	3DFF00	[Ar] 4s2	1.00	174	100 (+2)		590	-2	2	solid	metallic	1115	1757	1.55	alkaline earth metal	Ancient"
+Sc="Scandium	44.955912(6)	E6E6E6	[Ar] 3d1 4s2	1.36	144	74.5 (+3)		633	-18	1, 2, 3	solid	metallic	1814	3103	2.99	transition metal	1876"
+Ti="Titanium	47.867(1)	BFC2C7	[Ar] 3d2 4s2	1.54	136	86 (+2)		659	-8	-1, 2, 3, 4	solid	metallic	1941	3560	4.51	transition metal	1791"
+V="Vanadium	50.9415(1)	A6A6AB	[Ar] 3d3 4s2	1.63	125	79 (+2)		651	-51	-1, 2, 3, 4	solid	metallic	2183	3680	6.11	transition metal	1803"
+Cr="Chromium	51.9961(6)	8A99C7	[Ar] 3d5 4s1	1.66	127	80 (+2*)		653	-64	-2, -1, 1, 2, 3, 4, 5, 6	solid	metallic	2180	2944	7.14	transition metal	Ancient"
+Mn="Manganese	54.938045(5)	9C7AC7	[Ar] 3d5 4s2	1.55	139	67 (+2)		717	0	-3, -2, -1, 1, 2, 3, 4, 5, 6, 7	solid	metallic	1519	2334	7.47	transition metal	1774"
+Fe="Iron	55.845(2)	E06633	[Ar] 3d6 4s2	1.83	125	78 (+2*)		763	-16	-2, -1, 1, 2, 3, 4, 5, 6	solid	metallic	1811	3134	7.87	transition metal	Ancient"
 x=input("dame algo chido")
 if x==H:
-    print (H1)
-elif x==He:
-    print (He1)
-elif x==Li:
-    print (Li1)
-elif x==Be:
-    print (Be1)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
-elif x==He:
-    print (He2)
 
+    print (H1)
+else:
+    if x==He:
+        print (He1)
 y=raw_input("ya valiste??")
 if y==si:
     print("no  tuve tiepo chavos")
